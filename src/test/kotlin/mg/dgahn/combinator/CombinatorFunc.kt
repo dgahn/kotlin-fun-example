24745be58654ada545fcbd762012479caaf54ccf
dgahn <dgahn@si-analytics.ai>
2021-03-16 23:47:41 +0900

    filter 추가

--- src/test/kotlin/mg/dgahn/combinator/CombinatorFunc.kt
+++ src/test/kotlin/mg/dgahn/combinator/CombinatorFunc.kt
@@ -11,22 +11,31 @@ sealed class FunList<out T> {
 
 fun <T> FunList<T>.addHead(head: T): FunList<T> = FunList.Cons(head, this)
 
-tailrec fun <T> FunList<T>.reverse(acc: FunList<T> = FunList.Nil): FunList<T> = when(this) {
+tailrec fun <T> FunList<T>.reverse(acc: FunList<T> = FunList.Nil): FunList<T> = when (this) {
     FunList.Nil -> acc
     is FunList.Cons -> tail.reverse(acc.addHead(head))
 }
 
-tailrec fun <T> FunList<T>.appendTail(value: T, acc: FunList<T> = FunList.Nil): FunList<T> =  when (this) {
+tailrec fun <T> FunList<T>.appendTail(value: T, acc: FunList<T> = FunList.Nil): FunList<T> = when (this) {
     FunList.Nil -> FunList.Cons(value, acc).reverse()
     is FunList.Cons -> tail.appendTail(value, acc)
 }
 
-fun <T> FunList<T>.getTail(): FunList<T> = when(this) {
+fun <T> FunList<T>.getTail(): FunList<T> = when (this) {
     FunList.Nil -> throw NoSuchElementException()
     is FunList.Cons -> tail
 }
 
-fun <T> FunList<T>.getHead(): T = when(this) {
+fun <T> FunList<T>.getHead(): T = when (this) {
     FunList.Nil -> throw NoSuchElementException()
     is FunList.Cons -> head
+}
+
+tailrec fun <T> FunList<T>.filter(acc: FunList<T> = FunList.Nil, p: (T) -> Boolean): FunList<T> = when (this) {
+    FunList.Nil -> acc.reverse()
+    is FunList.Cons -> if (p(head)) {
+        tail.filter(acc.addHead(head), p)
+    } else {
+        tail.filter(acc, p)
+    }
 }
--- src/test/kotlin/mg/dgahn/combinator/CombinatorFuncTest.kt
+++ src/test/kotlin/mg/dgahn/combinator/CombinatorFuncTest.kt
@@ -1,6 +1,9 @@
 package mg.dgahn.combinator
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.collections.shouldBeIn
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldHave
 
 class CombinatorFuncTest : FunSpec({
 
@@ -16,6 +19,13 @@ class CombinatorFuncTest : FunSpec({
     test("FunList를 통해서 [1.0, 2.0, 3.0, 4.0, 5.0]를 가지는 intList를 생성할 수 있다.") {
         val list: FunList<Double> =
             FunList.Cons(11.0, FunList.Cons(2.0, FunList.Cons(33.0, FunList.Cons(4.0, FunList.Cons(5.0, FunList.Nil)))))
+    }
+
+    test("FunList을 Filtering 할수 있다.") {
+        val list: FunList<Double> =
+            FunList.Cons(11.0, FunList.Cons(2.0, FunList.Cons(33.0, FunList.Cons(4.0, FunList.Cons(5.0, FunList.Nil)))))
+
+        list.filter { it > 10.0 } shouldBe FunList.Cons(11.0, FunList.Cons(33.0, FunList.Nil))
     }
 
 })
